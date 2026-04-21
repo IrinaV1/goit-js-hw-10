7e31a0bae7c1cf968901870b3fe9d933f151bf33
Irina <vyustirina@gmail.com>
2026-04-22 00:19:43 +0300

    add onClose

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,26 +1,51 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
 const btn = document.querySelector('[data-start]');
 const input = document.querySelector('#datetime-picker');
+let userSelectedDate = null;
+// let isActive = false;
 
-let isActive = false;
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
+    userSelectedDate = selectedDates[0];
+    if (userSelectedDate < options.defaultDate) {
+      iziToast.show({
+        title: '⚠️',
+        message: 'Please choose a date in the future',
+        theme: 'dark',
+        // color: 'red',
+        backgroundColor: '#ff0000',
+        position: 'topRight',
+        timeout: 4000,
+        progressBar: false,
+        close: true,
+        closeOnClick: true,
+        transitionIn: 'fadeInDown',
+        transitionOut: 'fadeOutUp',
+      });
+      btn.classList.remove('isActive');
+    } else {
+      btn.classList.add('isActive');
+    }
+    console.log(options.defaultDate);
+
     console.log(selectedDates[0]);
   },
 };
 
-let userSelectedDate = null;
-if (userSelectedDate) {
-  window.alert('Please choose a date in the future');
-}
-
 flatpickr(input, options);
 btn.addEventListener('click', handleClick);
 
-function handleClick() {}
+function handleClick() {
+  if (userSelectedDate) {
+    btn.classList.remove('isActive');
+  }
+}
